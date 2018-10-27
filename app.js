60a791bdd7978f04e3c1901f3d81e92540dcca08
y-moriguchi <yuichiro-moriguchi@nifty.com>
2018-10-28 08:39:37 +0900

    --use-multibyte option

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
  **/
 var fs = require('fs'),
 	os = require('os'),
-	P = require("./parse-tex.js")({ multibyte: false }),
+	P = require("./parse-tex.js"),
 	F = require("./format-text.js");
 
 function trim(text) {
@@ -18,27 +18,37 @@ function trim(text) {
 	return result;
 }
 
-function replaceText(text, eol) {
+function replaceText(text, eol, option) {
+	var parseTex = P(option);
 	return text.replace(/\\\[([^\\]|\\[^\]])*\\\]/g, function(match, body) {
 		var ast, out;
-		ast = P(trim(match.substring(2, match.length - 2)));
-		out = F(ast, { multibyte: false });
+		ast = parseTex(trim(match.substring(2, match.length - 2)));
+		out = F(ast, option);
 		return eol + out.replace("\n", eol) + eol;
 	});
 }
 
 function usage() {
 	console.error("Frederica Ver. 0.0.0");
-	console.error("usage: frederica filename");
+	console.error("usage: frederica [-o output-file] [--use-multibyte] filename");
 }
 
 function parseOption() {
 	var argCount = 2,
-		result = {};
+		result;
+	result = {
+		multibyte: false
+	};
 	while(argCount < process.argv.length) {
 		if(process.argv[argCount] === "-o") {
 			result.output = process.argv[argCount + 1];
 			argCount += 2;
+		} else if(process.argv[argCount] === "--use-multibyte") {
+			result.multibyte = true;
+			argCount++;
+		} else if(process.argv[argCount].substring(0, 1) === "-") {
+			console.error("Unrecognized option: " + process.argv[argCount]);
+			process.exit(2);
 		} else {
 			result.input = process.argv[argCount];
 			argCount++;
@@ -62,10 +72,10 @@ function main() {
 		process.exit(2);
 	}
 	if(option.output) {
-		result = replaceText(text, os.EOL);
+		result = replaceText(text, os.EOL, option);
 		fs.writeFileSync(option.output, result);
 	} else {
-		result = replaceText(text, "\n");
+		result = replaceText(text, "\n", option);
 		console.log(result.replace(/\n$/, ""));
 	}
 }
